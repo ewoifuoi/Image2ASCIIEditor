92292644c651a4dbefea0e0aac98885d3dee24c2
ewoifuoi <hamimelons@126.com>
2022-10-28 19:05:21 +0800

    main page

---
+++ Image2ASCIIEditor/Common/Console.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows;
+using Microsoft.UI.Composition;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Image2ASCIIEditor.Common;
+public static class Console
+{
+    public static ListView console = null;
+
+    public static void log(string content)
+    {
+        if(console != null)
+        {
+            ListBoxItem l = new ListBoxItem();
+            l.Content = new TextBlock().Text = DateTime.Now.ToString("T") + " : " + content;
+            console.Items.Add(l);
+        }
+         
+    }
+}
--- Image2ASCIIEditor/MainWindow.xaml.cs
+++ Image2ASCIIEditor/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Image2ASCIIEditor.Common;
+using Console = Image2ASCIIEditor.Common.Console;
+using System.Runtime.InteropServices;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -21,22 +24,37 @@ namespace Image2ASCIIEditor;
 /// <summary>
 /// An empty window that can be used on its own or navigated to within a Frame.
 /// </summary>
-public sealed partial class MainWindow : Window
+public partial class MainWindow : Window
 {
+    [DllImport("User32", CharSet = CharSet.Unicode)]
+    static extern IntPtr GetSystemMetrics(int nIndex);
+
+
     public MainWindow()
     {
         this.InitializeComponent();
-        
+
+        Console.console = this.lv;
         this.ExtendsContentIntoTitleBar = true;  // enable custom titlebar
         this.SetTitleBar(AppTitleBar);
+        IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+        var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+        var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+        appWindow.Resize(new Windows.Graphics.SizeInt32 { Width = 1400, Height = 1000});
+
+        Console.log("开始测试");
+
+
 
     }
+
+
 
     private void nvSample_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
     {
         FrameNavigationOptions options = new FrameNavigationOptions();
         options.TransitionInfoOverride = args.RecommendedNavigationTransitionInfo;
-
+        
         string navItemTag = args.InvokedItemContainer.Tag.ToString();
         Type pageType = null;
 
@@ -49,6 +67,6 @@ public sealed partial class MainWindow : Window
         {
             return;
         }
-        contentFrame.NavigateToType(pageType, null, options);
+        
     }
 }
