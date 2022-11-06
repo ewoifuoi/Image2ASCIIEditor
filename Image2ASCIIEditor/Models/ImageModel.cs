add36ae7979b0ca42f83dd08bb2747ef0b11127b
ewoifuoi <hamimelons@126.com>
2022-11-06 20:36:21 +0800

    消除白色背景

--- Image2ASCIIEditor/Models/ImageModel.cs
+++ Image2ASCIIEditor/Models/ImageModel.cs
@@ -26,68 +26,96 @@ public class ImageModel
     private int Height;
     private bool Refreshed = false;
     private Image imageControl;
-    public string ImagePath;
+    public string ImagePath = null;
 
-    public List<List<Windows.UI.Color>> PixelList; 
+    private Bitmap imageBitmap;
+
+    public List<List<Windows.UI.Color>> PixelList;// RGB矩阵
+    public List<List<Rectangle>> RectangleList;
 
 
     public void CreateBitmap(ref Canvas canvas, int rate)
     {
         Color srcColor;
-        Bitmap srcBitmap = new Bitmap(ImagePath);
-        // 设置目标图片高度时需除2
-        Bitmap imageBitmap = new Bitmap(rate, Convert.ToInt32(Convert.ToDouble(srcBitmap.Height) * (Convert.ToDouble(rate) / Convert.ToDouble(srcBitmap.Width))) / 2);
-        Graphics g = Graphics.FromImage(imageBitmap);
-        g.DrawImage(srcBitmap, new System.Drawing.Rectangle(0, 0, imageBitmap.Width, imageBitmap.Height), new System.Drawing.Rectangle(0, 0,srcBitmap.Width, srcBitmap.Height),  GraphicsUnit.Pixel);
-
-        //var imageBitmap = new Bitmap(ImagePath);
-        Console.log(imageBitmap.Size.ToString());
-
-
-        canvas.Children.Clear();
-        for(int i = 0; i < imageBitmap.Height; i++)
+        RectangleList = new List<List<Rectangle>>();
+        PixelList = new List<List<Windows.UI.Color>>();
+        if(ImagePath != null) // 导入图片成功
         {
-            for(int j = 0; j < imageBitmap.Width; j++)
+            Bitmap srcBitmap = new Bitmap(ImagePath);
+            // 设置目标图片高度时需除2
+            imageBitmap = new Bitmap(rate, Convert.ToInt32(Convert.ToDouble(srcBitmap.Height) * (Convert.ToDouble(rate) / Convert.ToDouble(srcBitmap.Width))) / 2);
+            Graphics g = Graphics.FromImage(imageBitmap);
+            g.DrawImage(srcBitmap, new System.Drawing.Rectangle(0, 0, imageBitmap.Width, imageBitmap.Height), new System.Drawing.Rectangle(0, 0, srcBitmap.Width, srcBitmap.Height), GraphicsUnit.Pixel);
+
+            //var imageBitmap = new Bitmap(ImagePath);
+            Console.log(imageBitmap.Size.ToString());
+
+
+            canvas.Children.Clear();
+            for (int i = 0; i < imageBitmap.Height; i++)
             {
-                // 注 ：j为横坐标， i为纵坐标
-
-                // 目标图片高度已除2, 故不需改变矩形的高(矩形的高由计算得出)
-                Rectangle rect = new Rectangle();// 生成在画布里的矩形
-                if(imageBitmap.Height * 2 > imageBitmap.Width)
+                RectangleList.Add(new List<Rectangle>());
+                PixelList.Add(new List<Windows.UI.Color>());
+                for (int j = 0; j < imageBitmap.Width; j++)
                 {
-                    rect.Height = canvas.Height / imageBitmap.Height;
-                    rect.Width = rect.Height / 2;
-                }
-                else
-                {
-                    rect.Width = canvas.Height / imageBitmap.Width;// 矩形的宽
-                    rect.Height = rect.Width * 2;// 矩形的高
-                }
-                
-                srcColor = imageBitmap.GetPixel(j, i);// 取像素RGB
+                    // 注 ：j为横坐标， i为纵坐标
 
-                // 两种颜色类型的转换
-                rect.Fill = new SolidColorBrush(new Windows.UI.Color() {A=srcColor.A, R = srcColor.R, G = srcColor.G, B = srcColor.B });
-                Canvas.SetLeft(rect, j * rect.Width);
-                Canvas.SetTop(rect, i * rect.Height);
-                canvas.Children.Add(rect);
+                    // 目标图片高度已除2, 故不需改变矩形的高(矩形的高由计算得出)
+                    Rectangle rect = new Rectangle();// 生成在画布里的矩形
+                    if (imageBitmap.Height * 2 > imageBitmap.Width)
+                    {
+                        rect.Height = canvas.Height / imageBitmap.Height;
+                        rect.Width = rect.Height / 2;
+                    }
+                    else
+                    {
+                        rect.Width = canvas.Height / imageBitmap.Width;// 矩形的宽
+                        rect.Height = rect.Width * 2;// 矩形的高
+                    }
 
+                    srcColor = imageBitmap.GetPixel(j, i);// 取像素RGB
+                    PixelList[i].Add(new Windows.UI.Color() { A = srcColor.A, R = srcColor.R, G = srcColor.G, B = srcColor.B });
+
+                    // 两种颜色类型的转换
+                    rect.Fill = new SolidColorBrush(new Windows.UI.Color() { A = srcColor.A, R = srcColor.R, G = srcColor.G, B = srcColor.B });
+                    Canvas.SetLeft(rect, j * rect.Width);
+                    Canvas.SetTop(rect, i * rect.Height);
+                    RectangleList[i].Add(rect);
+                    canvas.Children.Add(rect);
+
+                }
             }
         }
+        
     }
 
-    public void showInRGB(ref Canvas canvas, int rate)
+    public void EraseWhiteBackground(ref Canvas testground, int rate)
     {
-    
+        if(RectangleList != null)
+        {
+            rate = 255 - rate;
+            for (int i = 0; i < RectangleList.Count; i++)
+            {
+                for (int j = 0; j < RectangleList[i].Count; j++)
+                {
+                    SolidColorBrush s;
+                    s = RectangleList[i][j].Fill as SolidColorBrush;
+                    Windows.UI.Color sc = s.Color;
+                    if (sc.R > rate && sc.G > rate && sc.B > rate)
+                    {
+                        RectangleList[i][j].Fill = new SolidColorBrush(Microsoft.UI.Colors.Transparent);
+                        PixelList[i][j] = Microsoft.UI.Colors.Transparent;
+                    }
+
+                }
+            }
+        }
+        
     }
 
-    public void showInGRAY(ref Canvas canvas, int rate)
-    {
-    
-    }
 
 
-    
+
     public bool isRefreshed{get { return Refreshed;}}
 
     /// <summary>
