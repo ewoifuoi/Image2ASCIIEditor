4b76f21e139dea57ead559e3dd547c810cd60647
ewoifuoi <hamimelons@126.com>
2022-11-10 21:47:50 +0800

    yanse

--- Image2ASCIIEditor/Models/ExportModel.cs
+++ Image2ASCIIEditor/Models/ExportModel.cs
@@ -9,6 +9,62 @@ public class ExportModel
 {
     private int op;
     private string outputForLinux;
+    private string ColorConvertForLinux(int id)
+    {
+        switch (id)
+        {
+            case 0:
+                return "\\033[1;30m";
+                break;
+            case 12:
+                return "\\033[1;31m";
+                break;
+            case 10:
+                return "\\033[1;32m";
+                break;
+            case 14:
+                return "\\033[1;33m";
+                break;
+            case 9:
+                return "\\033[1;34m";
+                break;
+            case 13:
+                return "\\033[1;35m";
+                break;
+            case 11:
+                return "\\033[1;36m";
+                break;
+            case 15:
+                return "\\033[1;37m";
+                break;
+
+
+            case 4:
+                return "\\033[0;31m";
+                break;
+            case 2:
+                return "\\033[0;32m";
+                break;
+            case 6:
+                return "\\033[0;33m";
+                break;
+            case 1:
+                return "\\033[0;34m";
+                break;
+            case 5:
+                return "\\033[0;35m";
+                break;
+            case 3:
+                return "\\033[0;36m";
+                break;
+            case 7:
+                return "\\033[0;30m";
+                break;
+
+            default:
+                return "";
+        }
+    }
     public ExportModel(int op)
     {
         this.op = op;
@@ -22,6 +78,7 @@ public class ExportModel
             {
                 for(int j = 0; j < StringStreamModel.charsList[i].Count; j++)
                 {
+
 
                 }
             }
